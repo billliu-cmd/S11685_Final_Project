0a67c238ce48cfb6ce96cb94a0bb3093fc136b3b
billliu-cmd <billliu@andrew.cmu.edu>
2026-04-16 17:23:51 -0400

    Update and rename x-trend.py to x_trend.py
    fix bugs

--- x_trend.py
+++ x_trend.py
@@ -2,7 +2,7 @@
 
 import torch, torch.nn as nn
 import torch.nn.functional as F
-from .components import (TemporalBlock, DecoderBlock, SelfAttention, CrossAttention, SideInfoFFN)
+from .components import TemporalBlock, DecoderBlock, SelfAttention, CrossAttention, SideInfoFFN
 from .config import MODEL
 
 class XTrend(nn.Module):
@@ -17,7 +17,7 @@ class XTrend(nn.Module):
         self.emb = nn.Embedding(num_assets, hid)
 
         # Encoder
-        self.encoder = TemporalBlock(input_dim, hid, num_assets,cfg["dropout"], self.emb)
+        self.encoder = TemporalBlock(input_dim, hid, num_assets, cfg["dropout"], self.emb)
         
         # Self-Attention
         self.ctx_self_attn = SelfAttention(hid, cfg["num_heads"], cfg["dropout"])
@@ -30,11 +30,11 @@ class XTrend(nn.Module):
 
         # Post-Attention/Pre-Decoder
         self.post_cross_ffn = nn.Sequential(nn.Linear(hid, hid), nn.ELU(), nn.Linear(hid, hid))
+        self.drop = nn.Dropout(cfg["dropout"])
         self.post_cross_norm = nn.LayerNorm(hid)
 
         # Decoder
-        self.drop = nn.Dropout(cfg["dropout"])
-        self.decoder = DecoderBlock(input_dim + 1, hid, num_assets, drop, self.emb)
+        self.decoder = DecoderBlock(input_dim, hid, num_assets, cfg["dropout"], self.emb)
         
         # Final Stage
         self.head = nn.Linear(hid, 1)
@@ -57,8 +57,8 @@ class XTrend(nn.Module):
 
         cross_out = self.post_cross_norm(self.drop(self.post_cross_ffn(cross_out)) + cross_out)
 
-        dec_in = torch.cat([target_x, target_y.unsqueeze(-1)], dim=-1)
-        dec_out = self.decoder(dec_in, target_id, cross_out)
+        dec_in = target_x
+        dec_out = self.decoder(dec_in, target_id, y)
 
         return torch.tanh(self.head(dec_out)).squeeze(-1)
       
